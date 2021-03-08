ca77ab118ac0e5107d43cccf4e5af93f54d868fd
snakemodule <Kasmano@gmail.com>
2021-03-08 15:06:48 +0100

    Revert "spawner WIP"
    This reverts commit 9f84a9f245d9d75d44211e68b60614465c2ac2e5.

--- Asteroids/Game.cpp
+++ Asteroids/Game.cpp
@@ -38,18 +38,16 @@ void Game::Update()
 }
 
 Game::Game(Window& window)
-	: _renderer(window), 
-	_spawnSystem(_asteroids, _projectiles, _aliens, _player, _asteroidsEnd, _projectilesEnd),
-	_collisionHandler(_renderer)
+	: _renderer(window), _spawnSystem(_asteroids, _projectiles, _aliens, _player), _collisionHandler(_renderer)
 {
+	
+
+
 	Asteroid* debugAsteroid = new Asteroid(50);
 	debugAsteroid->transform.SetPosition(100, 100); // change their positions here
 
 	
-	_asteroidsEnd = _asteroids.begin();
-	_projectilesEnd = _projectiles.begin();
-
-
+	_asteroids.reserve(32);
 	_asteroids = { *debugAsteroid };
 
 	_player.push_back(Player(400, 400, 6, 10));
--- Asteroids/SpawnSystem.h
+++ Asteroids/SpawnSystem.h
@@ -10,28 +10,17 @@ class SpawnSystem
 {
 public:
 
-	SpawnSystem(std::vector<Asteroid>& asteroids, std::vector<Projectile>& projectile, 
-		std::vector<Alien>& aliens, std::vector<Player>& player,
-		std::vector<Asteroid>::iterator& asteroidIterator, 
-		std::vector<Projectile>::iterator& projectileIterator);
+	SpawnSystem(std::vector<Asteroid>& asteroids, std::vector<Projectile>& projectile, std::vector<Alien>& aliens, std::vector<Player>& player);
 
 	/*
-	asteroid spawn perimeter	
+	asteroid spawn perimeter
+	
 	*/
 
-	void SpawnAsteroids();
-
-	void DestroyAsteroid(const int entity_id, int split);
-
-	Asteroid* memorySanityCheck;
+	//SpawnAsteroids()
 
 	std::vector<Asteroid>& _asteroids;
 	std::vector<Projectile>& _projectiles;
 	std::vector<Alien>& _aliens;
 	std::vector<Player>& _player;
-
-	std::vector<Asteroid>::iterator& _asteroidsEnd;
-	std::vector<Projectile>::iterator& _projectilesEnd;
-	//std::vector<Alien>::iterator _aliensEnd;
-	//std::vector<Player>::iterator _playerEnd;
 };
