7bdc6cc7096e051c27be6ce43ef40a8378fbd5ab
Flargy <marlun1996@hotmail.com>
2021-03-08 17:25:19 +0100

    Made the alien capable of chasing player

--- Asteroids/Alien.h
+++ Asteroids/Alien.h
@@ -1,7 +1,20 @@
 #pragma once
 #include "CollidableObject.h"
+#include "Player.h"
 
 class Alien : public CollidableObject
 {
-	Alien();
+public:
+	Alien(Player* player);
+	~Alien();
+
+	void CreatePoints();
+	void Collide();
+	void Update();
+	void Instantiate(Vector2 spawnposition);
+	void CalculateDirectionToPlayer();
+
+private:
+	Player* _player; // pointer to player for chasing purposes
+	double _speed = 2;
 };
--- Asteroids/Projectile.h
+++ Asteroids/Projectile.h
@@ -5,6 +5,7 @@
 class Projectile : public CollidableObject
 {
 public:
+	Projectile(int ID);
 	Projectile();
 	~Projectile(){}
 
