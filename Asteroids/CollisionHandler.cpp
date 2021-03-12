8af022a4b25ead4aea506bb7a0cb2dd95960a85c
Flargy <marlun1996@hotmail.com>
2021-03-12 14:52:22 +0100

    setup for ttf

--- Asteroids/CollisionHandler.cpp
+++ Asteroids/CollisionHandler.cpp
@@ -1,8 +1,4 @@
 #include "CollisionHandler.h"
-#include "Global.h"
-
-#define DOT(x1,y1,x2,y2)((x1 * x2) + (y1 * y2)) 
-
 
 
 bool BoundsTestLineSegment(float t) { return t >= 0 && t <= 1; };
@@ -57,75 +53,6 @@ struct DeferredFunctions {
 	}
 };
 
-bool CollisionHandler::CheckCollision(CollidableObject* obj, CollidableObject* otherObj, int iteration = 0, bool looped = false) {
-	//todo should returns true if the objects intersect, fine grained collision algorithm here.
-	//Algorithm is in place, just needs to be tested now
-
-	std::vector<Vector2>& points1 = *obj->GetPoints();
-	std::vector<Vector2>& points2 = *otherObj->GetPoints();
-	int size1 = points1.size();
-	int size2 = points2.size();
-	for (int i = 0; i < size1; i++)
-	{
-
-
-	Vector2 targetAxis;
-	int j = i + 1;
-	// gets reocurring values to avoid calling the Get functions multiple times.
-
-	
-
-	// calculates the axis which we will test, this is perpendicular to a side of the polygon
-	targetAxis.x = -(points1[j % size1].y - points1[i].y);
-	targetAxis.y = (points1[j % size1].x - points1[i].x);
-
-	double currentMagnitude = sqrt(pow(targetAxis.x, 2) + pow(targetAxis.y, 2));
-
-	if (currentMagnitude != 0)
-	{
-		targetAxis.x *= 1 / currentMagnitude;
-		targetAxis.y *= 1 / currentMagnitude;
-	}
-
-
-	double p1min = DOT(targetAxis.x, targetAxis.y, points1[0].x, points1[0].y);
-	double p1max = p1min;
-
-	for (int i = 0; i < size1; i++)
-	{
-		double dot = DOT(targetAxis.x, targetAxis.y, points1[i].x, points1[i].y);
-		p1min = fmin(p1min, dot);
-		p1max = fmax(p1max, dot);
-	}
-
-	std::array<double, 2> offset{ (obj->transform.GetPosition().x - otherObj->transform.GetPosition().x),  (obj->transform.GetPosition().y - otherObj->transform.GetPosition().y) };
-	double polyOffset = DOT(targetAxis.x, targetAxis.y, offset[0], offset[1]);
-	p1min += polyOffset;
-	p1max += polyOffset;
-	//---------------Correct-----------
-
-
-	double p2min = DOT(targetAxis.x, targetAxis.y, points2[0].x, points2[0].y);
-	double p2max = p1min;
-
-	for (int i = 0; i < size2; i++)
-	{
-		double dot = DOT(targetAxis.x, targetAxis.y, points2[i].x, points2[i].y);
-		p2min = fmin(p2min, dot);
-		p2max = fmax(p2max, dot);
-	}
-
-	if (p2max < p1min || p1max < p2min)
-	{
-		std::cout << "false" << std::endl;
-		return false;
-	}
-	}
-	
-	
-
-	return true;
-}
 
 std::array<double, 2> operator+(std::array<double, 2> left, std::array<double, 2> right) {
 	return std::array<double, 2>{ left[0]+right[0], left[1]+right[1] };
@@ -183,29 +110,16 @@ bool CollisionHandler::CheckCollisionV2(CollidableObject* obj, CollidableObject*
 }
 
 
-/*
-findAllCollisions(...)
-put asteroids in grid_map
-put alien in grid map
-collide bullets with asteroids&alien -> insert bullets in map
-collide player with entire map
 
-todo IMPORTANT??: handle duplicate collision callbacks!
-should keep a record of which objects have collided 
-and make sure checkCollision(...) returns false if already handled?
-*/
 void CollisionHandler::FindAllCollisions(
 	GameObjectBuffer<Asteroid, 32>& asteroids,
 	GameObjectBuffer<Projectile, 16>& bullets,
 	Player& player, Alien& alien, int gridSize) 
-	// think the gridsize should be an int as the size of the window is calculated in ints(pixels)
 {
 	map.clear();
 	
 	auto addToMap = [this, gridSize](CollidableObject& object, grid_cell cell)
 	{
-		//debugRenderer.DrawRect(gridSize, gridSize, gridSize * cell.first, gridSize * cell.second);
-
 
 		auto mapIterator = map.find(cell);
 		if (mapIterator == map.end())
